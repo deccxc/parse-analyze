f6eb7ab22cdf1f6894c8c5de56f6ae0555d44761
darrianhunter7 <62912771+darrianhunter7@users.noreply.github.com>
2020-09-18 12:47:59 -0500

    Update to make code way more efficient
    This update changed the code quite a bit by eliminating the printing out of all the logs. Instead, this code just prints out "The total number of requests made in the time period of this file is" with the amount at the end and "The total number of requests made in 1995 is" with the amount at the end. MUCH more efficient than the last code.

--- parse_analyze.py
+++ parse_analyze.py
@@ -2,14 +2,19 @@ from urllib.request import urlretrieve
 
 URL_PATH = 'https://s3.amazonaws.com/tcmg476/http_access_log'
 LOCAL_FILE = 'local_copy.log'
+total_requests = 0
+year_count = 0
 
 # Use urlretrieve() to fetch a remote copy and save into the local file path
 local_file, headers = urlretrieve(URL_PATH, LOCAL_FILE);
 
-count = 0
-f = open('local_copy.log')
-lines = f.readlines()
-for line in lines:
-    count += 1
-    print("Total requests made in time period on log:", count)
-    print(line.split()[3:4]);
+FILE_NAME = 'path/to/file'
+f = open(LOCAL_FILE)
+
+for line in f:
+    total_requests += 1
+    if '1995' in line:
+        year_count += 1
+
+print(f'The total number of requests made in the time period of this file is', total_requests)
+print(f'The total number of requests made in 1995 is', year_count)
