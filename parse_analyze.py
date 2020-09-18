4f9a9f2f85234926b16a49e797e3a065fe009e22
darrianhunter7 <62912771+darrianhunter7@users.noreply.github.com>
2020-09-18 10:43:38 -0500

    Allows the script to read through the log
    This update allows the script to read the log line for line but comes up with an error and is unable to read all the lines.

--- parse_analyze.py
+++ parse_analyze.py
@@ -5,3 +5,8 @@ LOCAL_FILE = 'local_copy.log'
 
 # Use urlretrieve() to fetch a remote copy and save into the local file path
 local_file, headers = urlretrieve(URL_PATH, LOCAL_FILE);
+
+f = open('local_copy.log')
+lines = f.readlines()
+for line in lines:
+    print(line.split()[3]);
