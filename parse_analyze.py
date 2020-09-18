8f2f943c37132b455760e464c8e610b6622a25ae
darrianhunter7 <62912771+darrianhunter7@users.noreply.github.com>
2020-09-18 12:55:54 -0500

    Updated comments in code
    I added comments to the code to explain what different parts of the code do.

--- parse_analyze.py
+++ parse_analyze.py
@@ -1,3 +1,4 @@
+#This portion of the code retrieves the log file from a network and saves a cached copy
 from urllib.request import urlretrieve
 
 URL_PATH = 'https://s3.amazonaws.com/tcmg476/http_access_log'
@@ -7,7 +8,8 @@ year_count = 0
 
 # Use urlretrieve() to fetch a remote copy and save into the local file path
 local_file, headers = urlretrieve(URL_PATH, LOCAL_FILE);
-
+#-------------------------------------------------------------------------------------------------
+#This portion of the code opens the log file and counts the total requests made in the time period and 1995
 FILE_NAME = 'path/to/file'
 f = open(LOCAL_FILE)
 
